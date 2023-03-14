c0641013697f82337e9a19fee88c603c832184d8
Arno Herrmann <arno.herrmann@newsfactory.de>
2023-03-14 17:44:02 +0100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# nginx_vts_build
+build the nginx geoIP module binary via docker on amazon linux with github actions
+
+![build](https://github.com/iamarno/nginx_geoIP_build/actions/workflows/build.yml/badge.svg)
---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+###versions
+
+nginx_version="1.23.1"
+module_version="3.4"
+
+# build nginx module
+echo "build with nginx version $nginx_version"
+
+# download nginx binary from nginx.org
+wget http://nginx.org/download/nginx-$nginx_version.tar.gz
+
+# extract
+tar xfv nginx-$nginx_version.tar.gz
+
+# download module source
+wget https://github.com/leev/ngx_http_geoip2_module/archive/refs/tags/$module_version.tar.gz
+
+# extract
+tar xfv v$module_version.tar.gz
+
+# cd
+cd nginx-$nginx_version
+
+# ./configure
+./configure --with-compat --add-dynamic-module=../ngx_http_geoip2_module-$module_version/ --with-cc-opt=-Wno-stringop-overread
+
+# make
+make
+
+# make install
+make install
+
+# copy module to module directory
+cp objs/ngx_http_vhost_traffic_status_module.so /module/.
+
+exit 0
